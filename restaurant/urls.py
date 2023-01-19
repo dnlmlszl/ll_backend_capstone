6fe98f51496b972589fda064d67ea0cb828503f0
László Mihály Dániel <daniel.mlaszlo@yahoo.com>
2023-01-19 18:00:32 +0100

    update files to repo

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from . import views
+from .views import BookingViewSet, MenuItemsView, SingleMenuItemView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('', views.home, name="home"),
     path("about", views.about, name="about"),
-    path("book", views.book, name="book"),
-    path("menu", views.menu, name="menu"),
-    path("menu_item/<int:pk>", views.display_menu_item, name="menu_item")
+    path("booking", views.BookingViewSet.as_view({'get': 'list', 'post': 'create', 'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
+    path("menu-items/", views.MenuItemsView.as_view()),
+    path("menu_items/<int:pk>", views.SingleMenuItemView.as_view()),
+    path('api-token-auth/', obtain_auth_token),
+    path('message/', views.msg),
+     path('api-token-auth/', obtain_auth_token),
 ]
